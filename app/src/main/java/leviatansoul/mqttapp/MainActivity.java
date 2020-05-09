dcba93687fd00c365d512c5a27e60e69fdaadbda
Diego <diego2.gd@gmail.com>
2020-05-09 22:13:36 +0200

    Mqtt optimal configuration

--- app/src/main/java/leviatansoul/mqttapp/MainActivity.java
+++ app/src/main/java/leviatansoul/mqttapp/MainActivity.java
@@ -12,8 +12,11 @@ import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
@@ -31,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     String acc_val = "0";
     String AZURE_IP = "51.140.222.237";
     String LOCAL_IP = "192.168.2.244";
+    String port = "1883";
+    String topic = "/Sensor/Accelerometer/x";
+    int delay = 2000;
 
 
     @Override
@@ -38,13 +44,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //UI Stuff
+
+        //Sensor Configuration
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        String clientId = MqttClient.generateClientId();
-        //client = new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.2.244:1883", clientId);
-        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://"+AZURE_IP+":1883", clientId);
+        //MQTT Configuration
+        String clientId = "SensorApp"+ System.currentTimeMillis();
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://"+AZURE_IP+":"+port, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true); //the client and server will not maintain state across restarts of the client, the server or the connection.
+        options.setAutomaticReconnect(true);
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    Log.d(TAG, "Reconnected to:");
+
+                    // Because Clean Session is true, we need to re-subscribe
+                   // subscribeToTopic(); Not needed yet
+                } else {
+                    Log.d(TAG, "Connected to: ");
+
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(TAG, "The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d(TAG, "Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
 
         try {
             IMqttToken token = client.connect();
@@ -68,6 +110,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
             });
         } catch (MqttException e) {
+            Log.d(TAG, "Client disconnected");
+
             e.printStackTrace();
         }
 
@@ -81,27 +125,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event){
-    Log.d(TAG, "onSensorChanged: X "+ event.values[0]);
+   // Log.d(TAG, "onSensorChanged: X "+ event.values[0]);
     acc_val = ""+event.values[0];
     }
 
     Runnable runnableTest =  new Runnable() {
         @Override
         public void run() {
-                while (true){
+            boolean exit = false;
+                while (!exit){
                     try {
-                        Thread.sleep(60);
-                        //Log.d("TIME", "50 mili-seconds");
+                        Thread.sleep(delay);
 
-                        String topic = "/Sensor/Accelerometer/x";
-                        String payload = acc_val+" "+System.currentTimeMillis();
-                        byte[] encodedPayload = new byte[0];
                         try {
-                            encodedPayload = payload.getBytes("UTF-8");
-                            MqttMessage message = new MqttMessage(encodedPayload);
-                            client.publish(topic, message);
+
+                            if (client.isConnected()){
+                                String payload = acc_val+" "+System.currentTimeMillis();
+                                MqttMessage message = new MqttMessage(payload.getBytes("UTF-8"));
+                                message.setQos(0);
+                                client.publish(topic, message);
+                                //message.setRetained(false); Default
+
+                            } else {
+                                exit = true;
+                            }
                         } catch (UnsupportedEncodingException | MqttException e) {
                             e.printStackTrace();
+                            Log.d(TAG, "MqttException in thread");
                         }
 
                     } catch (InterruptedException e) {
