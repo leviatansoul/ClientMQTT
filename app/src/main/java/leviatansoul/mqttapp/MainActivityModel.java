5c4b1bd84dccef64a3b2d0ee5e7d537cbddc69da
Diego <diego.garcia@appdynamics.com>
2020-09-25 19:01:24 +0200

    Few changes

--- app/src/main/java/leviatansoul/mqttapp/MainActivityModel.java
+++ app/src/main/java/leviatansoul/mqttapp/MainActivityModel.java
@@ -1,10 +1,7 @@
 package leviatansoul.mqttapp;
 
 
-import android.content.Context;
-import android.hardware.Sensor;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -12,7 +9,6 @@ import androidx.lifecycle.ViewModel;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import static android.content.ContentValues.TAG;
@@ -34,12 +30,9 @@ public class MainActivityModel extends ViewModel {
     }
 
     public void disconnectToBroker(final MqttClientManager mqttClientManager) {
-
-
         mqttClientManager.setStatus(false);
         isConnected.postValue(false);
-        mqttClientManager.disconnetClient();
-
+        mqttClientManager.disconnectClient();
     }
 
     public void connectToBroker(final MqttClientManager mqttClientManager, final GameSensorManager gameSensorManager, String ip, String port, final String user, final String frec) {
@@ -50,76 +43,72 @@ public class MainActivityModel extends ViewModel {
             //MQTT client is disconnected and thread is stopped
             mqttClientManager.setStatus(false);
             isConnected.postValue(false);
-            mqttClientManager.disconnetClient();
+            mqttClientManager.disconnectClient();
 
             Log.d(TAG, "Enter in disconnect");
 
         } else {
 
-            try {
                 /*
                  A new MQTT client is generated and connected to the broker
                  */
-                IMqttToken token = mqttClientManager.getClient().connect(mqttClientManager.getOptions());
-                token.setActionCallback(new IMqttActionListener() {
-                    @Override
-                    public void onSuccess(IMqttToken asyncActionToken) {
-                        // We are connected
-                        mqttClientManager.setStatus(true);
-                        isConnected.postValue(true);
-                        Log.d(TAG, "Client connected Succsefully");
+            IMqttToken token = mqttClientManager.connectClient(mqttClientManager.getOptions());
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    mqttClientManager.setStatus(true);
+                    isConnected.postValue(true);
+                    Log.d(TAG, "Client connected Succsefully");
 
 
-                        /**
-                         * Send concurrently the sensor values to the MQTT broker until the user stops the connection
-                         */
-                        new Thread(
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
+                    /**
+                     * Send concurrently the sensor values to the MQTT broker until the user stops the connection
+                     */
+                    new Thread(
+                            new Runnable() {
+                                @Override
+                                public void run() {
 
-                                        int frecuency = Integer.parseInt(frec);
+                                    int frecuency = Integer.parseInt(frec);
 
-                                        while (mqttClientManager.isClientConnected()) {
-                                            try {
+                                    while (mqttClientManager.isClientConnected()) {
+                                        try {
 
-                                                String topic = gameSensorManager.getSensorTopic(user);
-                                                MqttMessage message = mqttClientManager.prepareMQTTMessage(gameSensorManager.getSensorValue(), 0);
-                                                mqttClientManager.publishMessage(topic, message);
+                                            String topic = gameSensorManager.getSensorTopic(user);
+                                            MqttMessage message = mqttClientManager.prepareMQTTMessage(gameSensorManager.getSensorValue(), 0);
+                                            mqttClientManager.publishMessage(topic, message);
 
-                                                Thread.sleep(frecuency);
+                                            Thread.sleep(frecuency);
 
-                                            } catch (  InterruptedException e) {
-                                                e.printStackTrace();
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
 
-                                                mqttClientManager.setStatus(false);
-                                                isConnected.postValue(false);
-                                                Log.d(TAG, "Thread interrupted");
-                                            }
+                                            mqttClientManager.setStatus(false);
+                                            isConnected.postValue(false);
+                                            Log.d(TAG, "Thread interrupted");
                                         }
-                                        Log.d(TAG, "Connection finished" );
-
                                     }
+                                    Log.d(TAG, "Connection finished");
+
                                 }
-                        ).start();
+                            }
+                    ).start();
 
 
-                    }
+                }
 
-                    @Override
-                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                        // Something went wrong e.g. connection timeout or firewall problems
-                        Log.d(TAG, "onFailure - connection timeout or firewall problems");
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure - connection timeout or firewall problems");
 
-                        mqttClientManager.setStatus(false);
-                        isConnected.postValue(false);
+                    mqttClientManager.setStatus(false);
+                    isConnected.postValue(false);
 
-                    }
-                });
-            } catch (MqttException e) {
-                Log.d(TAG, "Client disconnected");
-                e.printStackTrace();
-            }
+                }
+            });
+
 
         }
 
--- app/src/main/java/leviatansoul/mqttapp/MqttClientManager.java
+++ app/src/main/java/leviatansoul/mqttapp/MqttClientManager.java
@@ -7,10 +7,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttToken;
 
 import java.io.UnsupportedEncodingException;
 
@@ -19,19 +21,19 @@ import static android.content.ContentValues.TAG;
 public class MqttClientManager {
 
 
-    private static MqttAndroidClient client;
-    private static String user, ip, port = "";
-    private static Context context;
-    private static boolean isClientConnected;
+    private MqttAndroidClient client;
+    private String user;
+    private  String ip;
+    private  String port;
+    private  boolean isClientConnected;
 
     public MqttClientManager(Context context, String ip, String port, String user) {
         this.user = user;
         this.ip = ip;
         this.port = port;
-        this.context = context;
         this.isClientConnected = false;
 
-        client = new MqttAndroidClient(context, "tcp://" + ip + ":" + port, user);
+        client = new MqttAndroidClient(context, "tcp://" + this.ip + ":" + this.port, this.user);
         client.setCallback(new MqttCallbackExtended() {
             @Override
             public void connectComplete(boolean reconnect, String serverURI) { //Called when the connection to the server is completed successfully.
@@ -66,60 +68,11 @@ public class MqttClientManager {
     }
 
 
-    public MqttAndroidClient getClient() {
-
-        return client;
-    }
-
-    /**
-     * This method generate a new MQTT client based in the configuration introduced in the UI
-     *
-     * @return MQTT Client object
-     */
-    public void setClient(Context c, String ip, String port, String user) {
-
-        this.client = new MqttAndroidClient(c, "tcp://" + ip + ":" + port, user);
-        this.client.setCallback(new MqttCallbackExtended() {
-            @Override
-            public void connectComplete(boolean reconnect, String serverURI) { //Called when the connection to the server is completed successfully.
-
-                if (reconnect) {
-                    Log.d(TAG, "Reconnected to:");
-
-                    // Because Clean Session is true, we need to re-subscribe
-                    // subscribeToTopic(); Not needed yet
-                } else {
-                    Log.d(TAG, "Connected to: ");
-
-                }
-            }
-
-            @Override
-            public void connectionLost(Throwable cause) {
-                Log.d(TAG, "The Connection was lost.");
-                isClientConnected = false;
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception { //This method is called when a message arrives from the server.
-                Log.d(TAG, "Incoming message: " + new String(message.getPayload()));
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) { //For QoS 0 messages it is called once the message has been handed to the network for delivery
-
-            }
-        });
-
-
-    }
-
     public boolean isClientConnected() {
         return isClientConnected;
     }
 
-    public void disconnetClient() {
-
+    public void disconnectClient() {
         //client.disconnect();
         client.close();
 
@@ -131,6 +84,16 @@ public class MqttClientManager {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+
+    public IMqttToken connectClient( MqttConnectOptions options){
+        try {
+            return client.connect(options);
+        } catch (MqttException e) {
+            e.printStackTrace();
+            Log.d(TAG, "Client disconnected");
+        }
+        return null;
     }
 
     public void setStatus(boolean clientConnected) {
@@ -155,6 +118,18 @@ public class MqttClientManager {
         }
 
         return message;
+    }
+
+    public  String getUser() {
+        return user;
+    }
+
+    public  String getIp() {
+        return ip;
+    }
+
+    public  String getPort() {
+        return port;
     }
 
 
